0b12c20eea185cc6bc39c44f6734852ea3d94ea1
Amber-Pittman <amber@amberpittman.com>
2019-08-22 21:58:15 -0400

    Started imports

--- src/data.js
+++ src/data.js
@@ -3,6 +3,11 @@
 // Export them as necessary and import each array into its appropriate
 // file. No real tricks here just be aware of what is in each array
 // and how you'll access the data.
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Index from "./index";
 
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
