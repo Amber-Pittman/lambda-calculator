cd8db245a0185e517f6e47aeb6a5784f7359acac
Amber-Pittman <amber@amberpittman.com>
2019-08-24 01:59:26 -0400

    Created special buttons

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const SpecialButton = (props) => {
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being 
       passed down from the parent container on props */}
       {props.special}
-    </>
+    </button>
   );
 };
 
