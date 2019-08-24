86e8814f796ca303884f90b54e65cda7a0ce5e42
Amber-Pittman <amber@amberpittman.com>
2019-08-24 01:21:02 -0400

    Added Operators into App to show errors in real time

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display numbers={0} />
         <Numbers />
+        <Operators />
       </div>
     </div>
   );
