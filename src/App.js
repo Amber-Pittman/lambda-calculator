b12b395d06ae9e24fd7af03b1a6c1b8c4f57dd0f
Amber-Pittman <amber@amberpittman.com>
2019-08-24 02:17:18 -0400

    Removed 0 from display

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Logo />
       <div className="App"> {/* prop */}
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Display numbers={0} />
+        <Display />
         <Numbers />
         <Operators />
         <Specials />
